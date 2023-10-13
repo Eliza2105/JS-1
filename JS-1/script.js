932ae9a2a275eaf9b3d8d604b921e60bb5feed28
AIDA9898 <138362134+Eliza2105@users.noreply.github.com>
2023-10-13 15:19:23 +0600

    Add files via upload

---
+++ JS-1/script.js
@@ -0,0 +1,25 @@
+alert( "salam")
+// 3,3,5,7.8 - Number - сан
+// "argen", "js","1332", string - сап
+// nan- сан эмес
+// null- ноль
+// underfined- табылбаган
+// boolean - falce/true
+// жалган/чын
+
+
+// var,let,const
+// case 
+// 1. canel
+// 2. cebab
+// 3. snike
+
+// let Fullname = "Argen"
+// Fullname = "Sezim"
+// let age = 30
+// console.log(Fullname);
+// console.log(age);
+
+// const name = 'A'
+// name "b"
+// console console.log(name);
